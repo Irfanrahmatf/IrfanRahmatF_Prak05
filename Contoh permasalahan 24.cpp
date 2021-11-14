0480cbd0b9d771db7deb52e5ac554b8dadf6bf77
Irfan Rahmat Firmansyah <90383815+Irfanrahmatf@users.noreply.github.com>
2021-11-14 11:46:37 +0700

    Add files via upload

---
+++ Contoh permasalahan 24.cpp
@@ -0,0 +1,17 @@
+/* Program sederhana menggunakan fungsi */
+/* yang tidak mengembalikan nilai */
+
+#include <stdio.h>
+#include <conio.h>
+void garis();				/* deklarasi fungsi */
+main()
+{	
+	garis();
+	printf("NO	NIM	 NILAI	NILAI\n");
+	garis();
+	getch();
+}
+void garis()				/* definisi fungsi*/
+{ 
+	printf ("=================================");
+}
